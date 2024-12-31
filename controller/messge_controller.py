b10e734273e54e7864491c6d69d6fe5426a4d743
Daedalus706 <uk070184@student.uni-kassel.de>
2024-12-31 17:20:40 +0100

    style

--- controller/messge_controller.py
+++ controller/messge_controller.py
@@ -6,12 +6,12 @@ class MessageController:
         self.event_queue = Queue()
         self.server = None
 
-    def send_message(self, turtle_id: int, message_type: str, data: dict | None) -> bool:
+    def send_message(self, turtle_id:int, message_type:str, data:dict|None) -> bool:
         if turtle_id not in self.server.connections.keys():
             return False
         return self.server.connections[turtle_id].send_data(message_type, data)
 
-    def broadcast_message(self, message_type: str, data: dict | None) -> bool:
+    def broadcast_message(self, message_type:str, data:dict|None) -> bool:
         message_sent = False
         for turtle_id, client in self.server.connections.items():
             message_sent = message_sent or client.send_data(message_type, data)
