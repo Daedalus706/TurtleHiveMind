b930fcf13e6fad36a0f04c04593fe862d6ac1007
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-02 15:05:27 +0100

    refactor: keep queue in message controller

--- controller/messge_controller.py
+++ controller/messge_controller.py
@@ -7,13 +7,22 @@ class MessageController:
         self.server = None
 
     def send_message(self, turtle_id:int, message_type:str, data:dict|None) -> bool:
-        if turtle_id not in self.server.connections.keys():
+        if turtle_id not in self.server.get_connection_keys():
             return False
-        return self.server.connections[turtle_id].send_data(message_type, data)
+        return self.server.get_connection(turtle_id).send_data(message_type, data)
 
     def broadcast_message(self, message_type:str, data:dict|None) -> set:
         message_sent = set()
-        for turtle_id, client in self.server.connections.items():
-            if client.send_data(message_type, data):
+        for turtle_id in self.server.get_connection_keys():
+            if self.server.get_connection(turtle_id).send_data(message_type, data):
                 message_sent.add(turtle_id)
         return message_sent
+
+    def add_event(self, new_event):
+        self.event_queue.put(new_event)
+
+    def get_event(self, block=True, timeout=None):
+        return self.event_queue.get(block, timeout)
+
+    def has_event(self):
+        return not self.event_queue.empty()
