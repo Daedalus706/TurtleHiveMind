55450f9a98ce80f9394aa17b7929b3b53c4c1f12
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-02 15:06:06 +0100

    add heartbeat

--- communication/client.py
+++ communication/client.py
@@ -3,8 +3,9 @@ import time
 from queue import Queue
 import threading
 
+from controller import MessageController
 from event import *
-
+from util import const
 from websockets import *
 from websockets.sync.server import ServerConnection
 
@@ -23,18 +24,28 @@ class ClientConnection:
 
 
 
-    def __init__(self, turtle_id:int, websocket:ServerConnection, event_queue:Queue):
-        self.id = turtle_id
+    def __init__(self, turtle_id:int, websocket:ServerConnection, message_controller:MessageController):
+        self.turtle_id = turtle_id
         self.websocket = websocket
-        self.event_queue = event_queue
+        self.message_controller = message_controller
         self.stop_event = threading.Event()
         self.active = True
         self.last_received = time.time()
 
         self.input_thread = threading.Thread(target=self._input_handler, daemon=True)
+        self.heartbeat_thread = threading.Thread(target=self._heartbeat_worker, daemon=True)
+
+    def _heartbeat_worker(self):
+        current_time = time.time()
+        if self.last_received + const.PING_INTERVAL < current_time:
+            self.ping()
+            time.sleep(1)
+        if self.last_received + const.TIMEOUT_INTERVAL < current_time:
+            print(f"Timeout on client {self.turtle_id}")
+            self.stop()
 
     def _input_handler(self):
-        print(f"Started Client id {self.id}")
+        print(f"Started Client id {self.turtle_id}")
         while not self.stop_event.is_set():
             data_dict = None
             try:
@@ -43,8 +54,7 @@ class ClientConnection:
                 if type(data) == str:
                     data_dict = json.loads(data)
             except ConnectionClosed:
-                self.active = False
-                self.stop_event.set()
+                self.stop()
 
             if data_dict is None:
                 continue
@@ -61,17 +71,22 @@ class ClientConnection:
             if new_event is None:
                 continue
 
-            self.event_queue.put(new_event)
+            self.message_controller.add_event(new_event)
 
     def start(self):
         self.input_thread.start()
+        self.heartbeat_thread.start()
 
     def stop(self):
         self.active = False
         self.stop_event.set()
         self.input_thread.join()
+        self.heartbeat_thread.join()
         self.websocket.close()
-        print(f"Closed Client connection {self.id}")
+        print(f"Closed Client connection {self.turtle_id}")
+
+    def ping(self):
+        self.send_data("ping", None)
 
     def send_data(self, message_type:str, payload:dict|None) -> bool:
         try:
