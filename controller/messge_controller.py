638221bef2f3f429eeb1829b4b081d00ee35ecd0
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-02 15:22:05 +0100

    add remove closed connections

--- controller/messge_controller.py
+++ controller/messge_controller.py
@@ -7,14 +7,14 @@ class MessageController:
         self.server = None
 
     def send_message(self, turtle_id:int, message_type:str, data:dict|None) -> bool:
-        if turtle_id not in self.server.get_connection_keys():
+        if turtle_id not in self.server.get_client_keys():
             return False
-        return self.server.get_connection(turtle_id).send_data(message_type, data)
+        return self.server.get_client(turtle_id).send_data(message_type, data)
 
     def broadcast_message(self, message_type:str, data:dict|None) -> set:
         message_sent = set()
-        for turtle_id in self.server.get_connection_keys():
-            if self.server.get_connection(turtle_id).send_data(message_type, data):
+        for turtle_id in self.server.get_client_keys():
+            if self.server.get_client(turtle_id).send_data(message_type, data):
                 message_sent.add(turtle_id)
         return message_sent
 
@@ -26,3 +26,6 @@ class MessageController:
 
     def has_event(self):
         return not self.event_queue.empty()
+
+    def close_connection(self, turtle_id:int):
+        self.server.close_connection(turtle_id)
