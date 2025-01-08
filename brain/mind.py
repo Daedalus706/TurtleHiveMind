b63f3844b489c876b5eb2b19ffb14d04efae3324
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-08 21:32:39 +0100

    add print events

--- brain/mind.py
+++ brain/mind.py
@@ -1,13 +1,15 @@
 import time
 from controller import MessageController
 
+
 class Mind:
 
     def __init__(self, message_controller:MessageController) -> None:
-        self.event_queue = message_controller.event_queue
         self.message_controller = message_controller
 
     def update(self):
         time.sleep(1)
+        for event in self.message_controller.get_events():
+            print(event)
         sent_to = self.message_controller.broadcast_message("empty", None)
         print("broadcast message to", sent_to)
