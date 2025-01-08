21ed7532ca13d84ed27e21b8b4ef8b62e4ef73c1
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-09 00:52:07 +0100

    add update turtle

--- model/turtle.py
+++ model/turtle.py
@@ -1,9 +1,22 @@
+import numpy as np
+from event import TurtleInfoEvent
 
 
 class Turtle:
 
-    def __init__(self, turtle_id:int, position:dict, direction:int):
+    def __init__(self, turtle_id:int):
         self.turtle_id = turtle_id
-        self.position = position
-        self.direction = direction
-        self.inventory = None
+        self.position:np.ndarray|None = None
+        self.direction:int|None = None
+        self.fuel:int|None = None
+        self.inventory:dict|None = None
+
+    def update_with_event(self, event:TurtleInfoEvent):
+        if event.position is not None:
+            self.position = event.position
+        if event.direction is not None:
+            self.direction = event.direction
+        if event.fuel is not None:
+            self.fuel = event.fuel
+        if event.inventory is not None:
+            self.inventory = event.inventory
