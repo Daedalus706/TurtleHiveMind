f14641da6e9f7611e7849f5e15f67f5824d342c3
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-14 23:04:03 +0100

    add datefmt to logger

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ def register_logger():
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
+        datefmt="[%Y-%m-%d %H:%M:%S]",
         handlers=[
             logging.FileHandler("server.log"),
             logging.StreamHandler()
