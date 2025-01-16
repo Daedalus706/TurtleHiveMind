389a037f9f79cb3bd5cc5777cbf4ea1445598ff6
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-16 12:33:32 +0100

    add logging

--- controller/model_controller.py
+++ controller/model_controller.py
@@ -20,6 +20,7 @@ class ModelController:
                 ModelController.create_save_folder(current_path, subfolders)
 
     def __init__(self):
+        self.logger = logging.getLogger("ServerLogger")
         self.saves_structure = {
             "saves": {
                 "chunks": {},
@@ -30,10 +31,11 @@ class ModelController:
         ModelController.create_save_folder('./', self.saves_structure)
 
     def purge(self):
+        self.logger.info("Purge Model")
         for root, dirs, files in os.walk("./saves"):
             for file in files:
                 file_path = os.path.join(root, file)
                 try:
                     os.remove(file_path)
                 except Exception as e:
-                    print(f"Error while purging files {file_path}: {e}")
+                    self.logger.error(f"Error while purging files {file_path}: {e}")
