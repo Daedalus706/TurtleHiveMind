33b8219a881e776de862db83279ad5d975fdfd02
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-16 14:13:34 +0100

    add config file

--- server.py
+++ server.py
@@ -1,29 +1,47 @@
 import logging
 import os
 from datetime import datetime
+import configparser
 
 from controller import *
 from communication import Server
 from brain import Mind
+from util import const
 
 
-def register_logger():
+def register_logger(log_level):
     if "logs" not in os.listdir():
-        os.mkdir("logs")
+        os.mkdir(const.LOG_FOLDER_PATH)
     logging.basicConfig(
-        level=logging.INFO,
+        level=log_level,
         format="%(asctime)s[%(levelname)s] %(message)s",
         datefmt="[%Y-%m-%d %H:%M:%S]",
         handlers=[
-            logging.FileHandler(f"logs/server{datetime.now().strftime('%Y-%m-%d')}.log"),
+            logging.FileHandler(f"{const.LOG_FOLDER_PATH}/server{datetime.now().strftime('%Y-%m-%d')}.log"),
             logging.StreamHandler()
         ]
     )
 
+def load_config() -> configparser.ConfigParser:
+    config_parser = configparser.ConfigParser()
+    if const.CONFIG_FILE_PATH not in os.listdir():
+        config_parser["Server"] = {
+            "ip": "127.0.0.1",
+            "port": "5020"
+        }
+        config_parser["Logger"] = {
+            "level": "INFO",
+        }
+        with open(const.CONFIG_FILE_PATH, "w") as configfile:
+            config_parser.write(configfile)
+    else:
+        config_parser.read(const.CONFIG_FILE_PATH)
+    return config_parser
 
 
 if __name__ == '__main__':
-    register_logger()
+    config = load_config()
+    register_logger(config.get("Logger", "level"))
 
     ## init controllers
     MessageController()
@@ -31,7 +49,7 @@ if __name__ == '__main__':
     ModelController()
     CraftingController()
 
-    server = Server(host='10.147.18.240', port=5020)
+    server = Server(host=config.get("Server", "ip"), port=config.getint("Server", "port"))
     mind = Mind()
 
     try:
--- util/const.py
+++ util/const.py
@@ -1,6 +1,9 @@
 PING_INTERVAL = 5
 TIMEOUT_INTERVAL = 10
 
+LOG_FOLDER_PATH = "logs"
+CONFIG_FILE_PATH = "settings.cfg"
+
 SAVES_STRUCTURE =   {
                         "saves": {
                         "chunks": {},
