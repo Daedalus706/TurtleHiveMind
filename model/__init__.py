9e9284caad6a3c0284847179c93fa54c0625cfb6
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-16 13:56:19 +0100

    add NoRecipe, to prevent calling create_recipe everytime an item with no recipe wants to know its recipe

--- model/__init__.py
+++ model/__init__.py
@@ -1,5 +1,5 @@
 from .turtle import Turtle
-from .recipe import Recipe
+from .recipe import Recipe, CraftingRecipe, NoRecipe
 from .item import Item
 from .inventory import Inventory
 from .chest import Chest
