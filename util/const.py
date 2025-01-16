b38e213108659ceaeb203cad4f5f026d166fb8f1
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-16 13:05:52 +0100

    moved SAVES_STRUCTURE to constants

--- util/const.py
+++ util/const.py
@@ -1,2 +1,10 @@
 PING_INTERVAL = 5
 TIMEOUT_INTERVAL = 10
+
+SAVES_STRUCTURE =   {
+                        "saves": {
+                        "chunks": {},
+                        "turtles": {},
+                        "chests": {}
+                        }
+                    }
