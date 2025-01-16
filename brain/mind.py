d3c55c73f05835a2793f73fb0793ede5f12d57bf
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-16 22:51:49 +0100

    replaced mind print with logger

--- brain/mind.py
+++ brain/mind.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 from controller import MessageController, CommandController, CraftingController
@@ -6,6 +7,7 @@ from controller import MessageController, CommandController, CraftingController
 class Mind:
 
     def __init__(self) -> None:
+        self.logger = logging.getLogger(__name__)
         self.message_controller = MessageController()
         self.command_controller = CommandController()
         self.crafting_controller = CraftingController()
@@ -13,5 +15,5 @@ class Mind:
     def update(self):
         time.sleep(1)
         for event in self.message_controller.get_events():
-            print(event)
+            self.logger.debug(f"Handle Event of type: {type(event)}")
         sent_to = self.message_controller.broadcast_message("empty", None)
