1b9153929b9074fdd1edee666cd08cd907def383
Daedalus706 <uk070184@student.uni-kassel.de>
2025-01-20 17:41:18 +0100

    update lau files

--- lua/online_turtle.lua
+++ lua/online_turtle.lua
@@ -87,7 +87,7 @@ local messageTable = {
 
 local function messageHandler(data)
     handled_messages = handled_messages + 1
-    print("handled_messages: " .. handled_messages .. "type: " .. tostring(data.type))
+    print("handleing: " .. tostring(data.type))
     if not data or type(data) ~= "table" then
         print("Invalid data received: " .. type(data))
     else
